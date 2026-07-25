e53450171275835843898546c93854d70a358076
OsmanAnk <osman.annak@hotmail.de>
2026-07-25 21:25:46 +0200

    Add CSS and JavaScript assets for the memory project
    - Introduced a new CSS file for styling the home and settings pages, including custom fonts and layout adjustments.
    - Added a JavaScript file to handle user interactions, including card flipping and choice selection logic.

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,15 @@ function init() {
     const playRef = document.getElementById("play")
     playRef?.addEventListener("click", startGame);
 
+    checkboxes("theme");
+    checkboxes("player");
+    checkboxes("board");
+
     const fieldRef = document.getElementById("field");
     if (fieldRef) {
         fieldRef.addEventListener("click", e => {
             const card = (e.target as HTMLElement).closest(".card") as HTMLButtonElement;
-            if(card) {
+            if (card) {
                 card.classList.toggle("is-flipped");
             }
         })
@@ -23,4 +27,21 @@ function startGame() {
 
     const settingsRef = document.getElementById("settings")
     settingsRef?.classList.remove("d_none")
+}
+
+function checkboxes(option: string) {
+    const checkboxes = document.querySelectorAll<HTMLInputElement>(".choices__item--" + option);
+    checkboxes.forEach((box) => {
+        box.addEventListener("change", (e) => {
+            const currentCheckbox = e.target as HTMLInputElement;
+
+            if (currentCheckbox.checked) {
+                checkboxes.forEach((other) => {
+                    if (other !== e.target) {
+                        other.checked = false;
+                    }
+                });
+            }
+        });
+    });
 }
