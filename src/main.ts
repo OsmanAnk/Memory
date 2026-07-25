8d5882b04db691a53abff72134081ea7b528e457
OsmanAnk <osman.annak@hotmail.de>
2026-07-25 22:15:35 +0200

    Add initial JavaScript and CSS files for game functionality and styling
    - Created index-7bsj6Y8J.js for handling game interactions, including theme selection and card flipping.
    - Added index-DW6NZ3GK.css for styling the game interface, including fonts, layout, and button effects.

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ function init() {
     checkboxes("theme");
     checkboxes("player");
     checkboxes("board");
+    hoverPreview();
 
     const fieldRef = document.getElementById("field");
     if (fieldRef) {
@@ -34,14 +35,66 @@ function checkboxes(option: string) {
     checkboxes.forEach((box) => {
         box.addEventListener("change", (e) => {
             const currentCheckbox = e.target as HTMLInputElement;
-
+            checkedImg(currentCheckbox)
             if (currentCheckbox.checked) {
                 checkboxes.forEach((other) => {
                     if (other !== e.target) {
                         other.checked = false;
+                        uncheckedImg(other)
                     }
                 });
             }
         });
     });
+}
+
+function checkedImg(currentCheckbox: HTMLInputElement) {
+    const themeName = currentCheckbox.id.replace("theme-", "")
+    const previewImg = document.getElementById("preview-" + themeName)
+
+    previewImg?.classList.remove("d_none");
+}
+
+function uncheckedImg(other: HTMLInputElement) {
+    const themeName = other.id.replace("theme-", "")
+    const previewImg = document.getElementById("preview-" + themeName)
+
+    previewImg?.classList.add("d_none");
+}
+
+function hoverPreview() {
+    const themeCheckboxes = document.querySelectorAll<HTMLInputElement>(".choices__item--theme");
+
+    themeCheckboxes.forEach((checkbox) => {
+        const label = checkbox.closest(".choices__label");
+
+        label?.addEventListener("mouseenter", () => {
+            showPreviewImg(checkbox);
+        });
+
+        leavePreview(label);
+    });
+}
+
+function leavePreview(label: Element | null) {
+    label?.addEventListener("mouseleave", () => {
+        const checkedTheme = document.querySelector<HTMLInputElement>(".choices__item--theme:checked");
+
+        if (checkedTheme) {
+            showPreviewImg(checkedTheme);
+        }
+    });
+}
+
+function showPreviewImg(checkbox: HTMLInputElement) {
+    const previewImages = document.querySelectorAll<HTMLElement>(".preview__img");
+
+    previewImages.forEach((img) => {
+        img.classList.add("d_none");
+    });
+
+    const themeName = checkbox.id.replace("theme-", "");
+    const previewImg = document.getElementById("preview-" + themeName);
+
+    previewImg?.classList.remove("d_none");
 }
